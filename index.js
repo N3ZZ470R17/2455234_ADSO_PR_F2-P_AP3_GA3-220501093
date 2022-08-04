8f8f1a5cc8c1093e487f0537b337632e0bfd985b
Rodney C <N3ZZ470R17@gmail.com>
2022-08-03 20:42:42 -0500

    Update

--- index.js
+++ index.js
@@ -1,34 +1,41 @@
-class Ahorcado{
-  constructor(){
-    this.error = 0;
-    this.jugadores = [];
-    this.bandera = false;
+class Ahorcado {
+    constructor() {
+        this.error = 0;
+        this.jugadores = [];
+        this.bandera = false;
+        this.partida = false;
 
-    this.palabras = ["complejo", "murcielago", "alcance", "ornitorrinco", "programacion"];
-    this.palabra = this.palabras[Math.floor(Math.random() * this.palabras.length)];
-    this.miPalabra = this.palabra.replace(/./g, "_ ");
-  }
-
-  iniciar(){
-    let num = 1;
-    
-    do{
-      this.jugadores.push(prompt(`Ingrese Jugador ${num}`));
-      this.bandera = prompt("Desea agregar mas jugadores?") == 'si' ? true : false;
-      num ++;
-    }while(this.bandera)
-
-    if(this.jugadores.length){
-      this.bandera = false;
-      console.log("**********************");
-      console.log("COMENCEMOS EL JUEGO");
-      console.log("Encuentra la palabra escondida, tienes 3 intentos");
-
-      console.log(this.miPalabra)
+        this.palabras = ["complejo", "murcielago", "alcance", "ornitorrinco", "programacion"];
+        this.palabra = this.palabras[Math.floor(Math.random() * this.palabras.length)];
+        this.miPalabra = this.palabra.replace(/./g, "_ ");
     }
 
-    //Apartir de aca el juego
-  }
+    iniciar() {
+        let num = 1;
+
+        do {
+            this.jugadores.push(prompt(`Ingrese Jugador ${num}`));
+            this.bandera = prompt("Desea agregar mas jugadores?") == 'si' ? true : false;
+            num++;
+        } while (this.bandera)
+
+        if (this.jugadores.length) {
+            this.bandera = false;
+            console.log("**********************");
+            console.log("COMENCEMOS EL JUEGO");
+            console.log("Encuentra la palabra escondida, tienes 3 intentos");
+
+            console.log(this.miPalabra)
+        }
+
+        //Apartir de aca el juego
+
+        while (this.partida == true) {
+
+
+        }
+
+    }
 }
 
 
